c73a305e9673cffc7ad161aba023c731fa39c031
unknown <rizkyferdian002@gmail.com>
2022-07-14 13:13:51 +0700

    new Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:airplane/cubit/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/get_started.dart';
 import 'package:airplane/ui/pages/main_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +16,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => SplashScreen(),
-        '/get-started': (context) => GetStarted(),
-        '/sign-up': (context) => SignUpPage(),
-        '/bonus': (context) => BonusPage(),
-        '/main': (context) => MainPage(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/get-started': (context) => GetStarted(),
+          '/sign-up': (context) => SignUpPage(),
+          '/bonus': (context) => BonusPage(),
+          '/main': (context) => MainPage(),
+        },
+      ),
     );
   }
 }
--- lib/ui/pages/checkout.dart
+++ lib/ui/pages/checkout.dart
@@ -1,166 +1,296 @@
 import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/pages/success_checkout.dart';
 import 'package:airplane/ui/widget/booking_detail_item.dart';
+import 'package:airplane/ui/widget/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({Key? key}) : super(key: key);
-
-  Widget route() {
-    return Container(
-      margin: EdgeInsets.only(top: 50),
-      child: Column(
-        children: [
-          Container(
-            width: 291,
-            height: 65,
-            margin: EdgeInsets.only(bottom: 10),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/image_checkout.png"),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "CGK",
-                    style: blackTextStyle.copyWith(
-                        fontSize: 24, fontWeight: semiBold),
-                  ),
-                  Text(
-                    "Tangerang",
-                    style: greyTextStyle.copyWith(fontWeight: light),
-                  ),
-                ],
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    "TLC",
-                    style: blackTextStyle.copyWith(
-                        fontSize: 24, fontWeight: semiBold),
-                  ),
-                  Text(
-                    "Ciliwung",
-                    style: greyTextStyle.copyWith(fontWeight: light),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget bookingDetails() {
-    return Container(
-      margin: EdgeInsets.only(top: 30),
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-      decoration: BoxDecoration(
-        color: kWhiteColor,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 70,
-                width: 70,
-                margin: EdgeInsets.only(right: 16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(defaultRadius),
-                  image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: AssetImage(
-                      "assets/image_destination1.png",
-                    ),
-                  ),
+  @override
+  Widget build(BuildContext context) {
+    Widget route() {
+      return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Container(
+              width: 291,
+              height: 65,
+              margin: EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/image_checkout.png"),
                 ),
               ),
-              Expanded(
-                child: Column(
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Lake Ciliwung',
-                      style: blackTextStyle.copyWith(fontSize: 18),
+                      "CGK",
+                      style: blackTextStyle.copyWith(
+                          fontSize: 24, fontWeight: semiBold),
                     ),
-                    Text('Tangerang')
+                    Text(
+                      "Tangerang",
+                      style: greyTextStyle.copyWith(fontWeight: light),
+                    ),
                   ],
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(right: 3),
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/icon_star.png"),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "TLC",
+                      style: blackTextStyle.copyWith(
+                          fontSize: 24, fontWeight: semiBold),
+                    ),
+                    Text(
+                      "Ciliwung",
+                      style: greyTextStyle.copyWith(fontWeight: light),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget bookingDetails() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: EdgeInsets.only(right: 16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(defaultRadius),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        "assets/image_destination1.png",
                       ),
                     ),
                   ),
-                  Text(
-                    '4.8',
-                    style: blackTextStyle.copyWith(
-                      fontWeight: semiBold,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Lake Ciliwung',
+                        style: blackTextStyle.copyWith(fontSize: 18),
+                      ),
+                      Text('Tangerang')
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 3),
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/icon_star.png"),
+                        ),
+                      ),
                     ),
-                  )
+                    Text(
+                      '4.8',
+                      style: blackTextStyle.copyWith(
+                        fontWeight: semiBold,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            // Note: Title
+            Container(
+              margin: EdgeInsets.only(
+                top: 20,
+              ),
+              child: Text(
+                'Booking Details',
+                style:
+                    blackTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+              ),
+            ),
+
+            // NOTE : Item
+            BookingDetailItem(
+                title: "Traveler",
+                valueText: "2 Person",
+                valueColor: kBlackColor),
+
+            BookingDetailItem(
+                title: "Seat", valueText: "A3 B3", valueColor: kBlackColor),
+            BookingDetailItem(
+                title: "Insurance", valueText: "YES", valueColor: kGreenColor),
+            BookingDetailItem(
+                title: "Refundable", valueText: "No", valueColor: kRedColor),
+            BookingDetailItem(
+                title: "VAT", valueText: "45%", valueColor: kBlackColor),
+            BookingDetailItem(
+                title: "Price",
+                valueText: "IDR 8.500.690",
+                valueColor: kBlackColor),
+            BookingDetailItem(
+                title: "Grand Total",
+                valueText: "IDR 12.000.000",
+                valueColor: kPrimaryColor),
+          ],
+        ),
+      );
+    }
+
+    Widget paymentDetails() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: kWhiteColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Payment Details',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 70,
+                    margin: EdgeInsets.only(right: 16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          'assets/image_card.png',
+                        ),
+                      ),
+                    ),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 24,
+                            height: 24,
+                            margin: EdgeInsets.only(right: 6),
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage(
+                                  'assets/icon_plane.png',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Pay',
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 16,
+                              fontWeight: medium,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'IDR 80.400.000',
+                          style: blackTextStyle.copyWith(
+                            fontSize: 18,
+                            fontWeight: medium,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Current Balance',
+                          style: greyTextStyle.copyWith(
+                            fontWeight: light,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ],
-          ),
-          // Note: Title
-          Container(
-            margin: EdgeInsets.only(
-              top: 20,
             ),
-            child: Text(
-              'Booking Details',
-              style:
-                  blackTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),
+          ],
+        ),
+      );
+    }
+
+    Widget payNowButton() {
+      return CustomButton(
+        title: 'Pay Now',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) => const SuccessCheckoutPage()),
             ),
+          );
+        },
+        margin: EdgeInsets.only(top: 30),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(
+          top: 50,
+          bottom: 75,
+        ),
+        child: Text(
+          "Terms and Conditions",
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
           ),
+        ),
+      );
+    }
 
-          // NOTE : Item
-          BookingDetailItem(
-              title: "Traveler",
-              valueText: "2 Person",
-              valueColor: kBlackColor),
-
-          BookingDetailItem(
-              title: "Seat", valueText: "A3 B3", valueColor: kBlackColor),
-          BookingDetailItem(
-              title: "Insurance", valueText: "YES", valueColor: kGreenColor),
-          BookingDetailItem(
-              title: "Refundable", valueText: "No", valueColor: kRedColor),
-          BookingDetailItem(
-              title: "VAT", valueText: "45%", valueColor: kBlackColor),
-          BookingDetailItem(
-              title: "Price",
-              valueText: "IDR 8.500.690",
-              valueColor: kBlackColor),
-          BookingDetailItem(
-              title: "Grand Total",
-              valueText: "IDR 12.000.000",
-              valueColor: kPrimaryColor),
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: kBackgroundColor,
         body: ListView(
@@ -168,6 +298,9 @@ class CheckoutPage extends StatelessWidget {
           children: [
             route(),
             bookingDetails(),
+            paymentDetails(),
+            payNowButton(),
+            tacButton(),
           ],
         ));
   }
